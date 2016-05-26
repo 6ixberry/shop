fc6989390645a1fa6c5fb95ebef04095e3fe26bb
Aliyah Cottle <aliyahrcottle@gmail.com>
2016-05-26 14:39:14 -0400

    added scroll to top functionality

--- index.php
+++ index.php
@@ -384,7 +384,7 @@
 			</div>
 		</div>
 
-		<div class="top-tag">
+		<div class="scrollToTop">
 			<a href="#row-1">Top</a>
 		</div>
 	</div>
@@ -392,10 +392,21 @@
 <script>
 	$(function() {
 
-		$(document).scroll(function(){
-				$(".top-tag").fade( "fast", function() {
-  			});
-		});	
+		//Check to see if the window is top if not then display button
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+				$('.scrollToTop').fadeIn();
+			} else {
+				$('.scrollToTop').fadeOut();
+			}
+		});
+	
+		//Click event to scroll to top
+		$('.scrollToTop').click(function(){
+			$('html, body').animate({scrollTop : 0},800);
+			return false;
+		});
+
 
 				
 	});
